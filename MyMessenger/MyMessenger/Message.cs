f09cf76149a4310acccd2303e93841ae2633d13e
KatyaDremliuh <nerona.91@gmail.com>
2022-01-09 00:35:02 +0300

    Add Json-format for messages

---
+++ MyMessenger/MyMessenger/Message.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyMessenger
+{
+    [Serializable] 
+    public class Message // для сериализации класс должен быть public
+    {
+        public string UserName { get; set; }
+        public string MessageText { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        public Message(string userName, string messageText, DateTime timeStamp)
+        {
+            UserName = userName;
+            MessageText = messageText;
+            TimeStamp = timeStamp;
+        }
+
+        // ctor по умолчанию
+        public Message()
+        {
+            UserName = "System";
+            MessageText = "Server is running...";
+            TimeStamp = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            string output = $"{UserName} <{TimeStamp}>: {MessageText}";
+            return output;
+        }
+    }
+}
---
+++ MyMessenger/MyMessenger/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace MyMessenger
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Message msg = new Message("RR", "Privet!", DateTime.UtcNow);
+            //Console.WriteLine(msg.ToString());
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+        }
+    }
+}
